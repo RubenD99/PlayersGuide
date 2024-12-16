0bcae8df96874628e50228c073a21b999e8f8e9d
Ruben <ruben.g.davis@gmail.com>
2024-12-16 11:30:02 +0100

    players Guide tot challenge 22

--- src/Challenge22/playersGuide22.java
+++ src/Challenge22/playersGuide22.java
@@ -27,8 +27,107 @@ to randomly place the Manticore so that it can be a single-player game.
 
 package Challenge22;
 
+
+import java.util.Scanner;
+
 public class playersGuide22 {
+    int shots = 0;
+    int mantHealth = 10;
+    int cityHealth = 15;
+
+
+
     public static void main(String[] args) {
 
+        playersGuide22 varis = new playersGuide22();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(distanceFromCity());
+        spaces();
+
+        for (int ronden = 0; varis.mantHealth >= 0; ronden++ ) {
+            System.out.print("Ronden : " + ronden + "\n");
+            System.out.print("City Health : " + varis.cityHealth );
+            System.out.print(" Manticore Health : " + varis.mantHealth + "\n");
+
+
+            //System.out.println(cannon(varis.shots));
+
+
+            //varis.shots++;
+
+            varis.mantHealth--;
+        }
     }
+
+
+    static int distanceFromCity (){
+                                                                    // methode voor het vragen van de afstand van het schip
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" wat is de afstand van het schip? (geef nummer tussen 0 - 100");
+        int distance = input.nextInt();
+        do {
+
+            System.out.println("gekozen afstand is niet correct. \n kies een nieuwe afstand tussen 0 - 100");
+            distance = input.nextInt();
+
+
+        }while (distance < 0 || distance > 100);
+
+        return distance;
+    }
+
+
+    static void spaces (){
+        for (int i = 0; i <= 50; i++){          // methode voor het legen van de scherm
+            System.out.println(" ");
+
+        }
+    }
+
+    static int vraagAfstand (int afstand){
+
+        return afstand;
+    }
+
+
+
+    static String cannon (int schoten){
+                                                    // shots and damage
+
+        int rate3 = schoten % 3;
+        int rate5 = schoten % 5;
+
+        if (rate3 == 0 && rate5 == 0){
+            return "10 damage";
+        }else {
+
+
+            if (rate3 == 0) {
+            return "3 damage";
+            } else if (rate5 == 0) {
+            return "3 damage";
+            }
+        }
+        return "1 damage";
+    }
+
+
 }
+
+/*
+ playersGuide22 game = new playersGuide22();
+        game.setDistance();
+        spaces();
+        game.cannon();
+ */
+/*playersGuide22 game2 = new playersGuide22();
+        game2.setDistance();
+        spaces(); */
+// if args.length != 0
+//   throw tooManyArgsException
+//
+// Program program = new Program
+// program.run();
+// program.shutdown()
